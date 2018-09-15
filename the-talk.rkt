7377755a9733e52fabdd80645785033207ea9f60
Caner Derici <canerderici@gmail.com>
2018-09-15 18:08:11 -0400

    the outline

--- the-talk.rkt
+++ the-talk.rkt
@@ -1,7 +1,8 @@
 #lang slideshow
 
 (require (for-syntax syntax/parse)
-         (rename-in slideshow (slide orig-slide)))
+         (rename-in slideshow (slide orig-slide))
+         slideshow/code)
 
 (define slide-count 0)
 
@@ -15,3 +16,38 @@
 (current-page-number-adjust
  (λ (str n)
    (format "~a / ~a" n slide-count)))
+
+(define outline
+  (let ([sub-para
+         (lambda l
+           (para #:width (* 3/4 (current-para-width)) l))])
+    (make-outline
+     'one "Part I: Modeling Linklets Using PLT Redex"
+     #f
+
+     'two "Part II: Adding Pairs to the Denotational Model"
+     (lambda (tag)
+       (sub-para "... of the CBV λ-calculus"))
+
+     'three "Part III: Measuring Pycket's Performance"
+     (lambda (tag)
+       (sub-para "... with the new linklets"))
+
+     'end "Conclusion"
+     #f)))
+
+(slide
+ #:title "Qualifying Examination Response"
+ (t "Caner Derici"))
+
+(outline 'one)
+
+(outline 'two)
+
+(outline 'three)
+
+(outline 'end)
+
+;Adding Pairs To The Denotational Model Of The Untyped CBV Lambda Calculus
+
+;Measuring The Performance Of The Linklet System On Pycket
