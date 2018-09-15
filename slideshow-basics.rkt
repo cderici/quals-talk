9f95ef5218f3de6f19c2f0640a8ad2559bc38e92
Caner Derici <canerderici@gmail.com>
2018-09-15 15:29:52 -0400

    playing with the page numbers

--- slideshow-basics.rkt
+++ slideshow-basics.rkt
@@ -1,5 +1,21 @@
 #lang slideshow
 
+(require (for-syntax syntax/parse)
+         (rename-in slideshow (slide orig-slide)))
+
+(define slide-count 0)
+
+(define-syntax slide
+  (syntax-parser
+    [(_ arg ...)
+     #'(begin
+         (set! slide-count (add1 slide-count))
+         (orig-slide arg ...))]))
+
+(current-page-number-adjust
+ (λ (str n)
+   (format "~a / ~a" n slide-count)))
+
 (slide
  #:title "How to Say Hello"
  (t "Hello World!"))
