42a18282f05ffff64e1956fd3637e0a511497b32
Caner Derici <canerderici@gmail.com>
2018-09-15 19:54:04 -0400

    the title page

--- the-talk.rkt
+++ the-talk.rkt
@@ -2,9 +2,58 @@
 
 (require (for-syntax syntax/parse)
          (rename-in slideshow (slide orig-slide))
-         slideshow/code)
+         slideshow/code
+         slideshow/base
+         pict
+         racket/draw)
 
-(define slide-count 0)
+(define slide-count 4) ; 4 outline minus the title
+
+;; A pict to use behind the main content
+(define fade-bg
+  (let ([w (+ (* 2 margin) client-w)]
+        [h (+ (* 2 margin) client-h)]
+        [trans (make-object brush% "white" 'transparent)]
+        [inside (make-object brush% "white" 'solid)])
+    (inset (dc (lambda (dc x y)
+                 (let ([b (send dc get-brush)]
+                       [p (send dc get-pen)]
+                       [draw-one
+                        (lambda (i)
+                          (send dc draw-rectangle
+                                (+ x i) (+ y i)
+                                (- w (* 2 i)) (- h (* 2 i))))])
+                   (send dc set-brush trans)
+                   (color-series 
+                    dc margin 1
+                    (make-object color% "black")
+                    (make-object color% "white")
+                    draw-one
+                    #t #t)
+                   (send dc set-brush inside)
+                   (draw-one margin)
+                   (send dc set-pen p)
+                   (send dc set-brush b)))
+               w h 0 0)
+           (- margin))))
+
+(current-slide-assembler
+ (lambda (s v-sep c)
+   (lt-superimpose
+    (lbl-superimpose
+     fade-bg
+     (hc-append (scale (bitmap (build-path "images" "plt-logo.png")) 0.3)
+                (scale (bitmap (build-path "images" "isabelle.png")) 0.3)
+                (scale (bitmap (build-path "images" "pycket.png")) 0.3)))
+    (let ([c (colorize c "darkred")])
+      (if s
+          (vc-append v-sep 
+                     ;; left-aligns the title:
+                     (ghost (scale (titlet s) 2))
+                     (titlet s)
+                     c)
+          c))
+      )))
 
 (define-syntax slide
   (syntax-parser
@@ -36,11 +85,29 @@
      'end "Conclusion"
      #f)))
 
-(slide
- #:title "Qualifying Examination Response"
- (t "Caner Derici"))
+(set-page-numbers-visible! #f)
+
+ (slide
+  #:title "Qualifying Examination Response"
+  (scale (t "Caner Derici") 0.8)
+  (scale (t "19 Sep 2018") 0.5)
+  (blank)
+  (scale (it "Advisor:") 0.5)
+  (scale (it "Sam Tobin-Hochstadt") 0.5)
+  )
+
+#;(start-at-recent-slide)
+
+#;(set-page-numbers-visible! #t)
 
 (outline 'one)
+
+
+
+ (slide
+  #:title "Linklets"
+  (t "linklets")
+  )
 
 (outline 'two)
 
